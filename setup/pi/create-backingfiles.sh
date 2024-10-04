ff71e6af76fa34105d6d3b23b5e489904dbea9e8
Marco Nelissen <marco.nelissen@gmail.com>
2024-10-03 18:34:39 -0700

    setup: resize, add or remove images when config is changed
    Update disk images when their size is changed in the config file.
    Disk images may be removed by setting their previously-nonzero size to 0,
    created by setting their previously-0 size to nonzero, or recreated with
    a different size by changing the size in the config file. An image will
    remain unchanged if its size is not changed.

--- setup/pi/create-backingfiles.sh
+++ setup/pi/create-backingfiles.sh
@@ -46,9 +46,9 @@ function is_percent() {
   echo "$1" | grep '%' > /dev/null
 }
 
-available_space () {
-  freespace=$(df --output=avail --block-size=1K "$BACKINGFILES_MOUNTPOINT/" | tail -n 1)
-  # leave 10 GB of free space for filesystem bookkeeping and snapshotting
+function available_space () {
+  freespace=$(df --output=size --block-size=1K "$BACKINGFILES_MOUNTPOINT/" | tail -n 1)
+  # reserve 10 GB for filesystem bookkeeping and snapshotting
   # (in kilobytes so 10M KB)
   padding=$(dehumanize "10M")
   echo $((freespace-padding))
@@ -56,13 +56,6 @@ available_space () {
 
 function calc_size () {
   local requestedsize="${!1}"
-  local availablesize
-  availablesize="$(available_space)"
-  if [ "$availablesize" -lt 0 ]
-  then
-    echo "0"
-    return
-  fi
   if is_percent "$requestedsize"
   then
     case ${1} in
@@ -86,10 +79,6 @@ function calc_size () {
     log_progress "Percentage-based size no longer supported, using default size of $requestedsize for $1" > /dev/stderr
   fi
   requestedsize="$(( $(dehumanize $requestedsize) / 1024 ))"
-  if [ "$requestedsize" -gt "$availablesize" ]
-  then
-    requestedsize="$availablesize"
-  fi
   echo "$requestedsize"
 }
 
@@ -99,9 +88,23 @@ function add_drive () {
   local size="$3"
   local filename="$4"
   local useexfat="$5"
+  local mountpoint=/mnt/"$name"
+
+  if image_matches_params "$filename" "$size" &> /dev/null
+  then
+    return
+  fi
+
+  if [ "$size" -eq "0" ]
+  then
+    rm -f "$filename" &> /dev/null
+    rm -f "$filename.opts" &> /dev/null
+    rmdir "$mountpoint" &> /dev/null || true
+    return
+  fi
 
   log_progress "Allocating ${size}K for $filename..."
-  fallocate -l "$size"K "$filename"
+  truncate --size="$size"K "$filename"
   if [ "$useexfat" = true  ]
   then
     echo "type=7" | sfdisk "$filename" > /dev/null
@@ -122,7 +125,6 @@ function add_drive () {
   fi
   losetup -d "$loopdev"
 
-  local mountpoint=/mnt/"$name"
 
   if [ ! -e "$mountpoint" ]
   then
@@ -146,40 +148,63 @@ function check_for_exfat_support () {
   fi
 }
 
-CAM_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/cam_disk.bin"
-MUSIC_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/music_disk.bin"
-LIGHTSHOW_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/lightshow_disk.bin"
-BOOMBOX_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/boombox_disk.bin"
-
-# delete existing files, because fallocate doesn't shrink files, and
-# because they interfere with the free-space calculation
-if [ -e "$CAM_DISK_FILE_NAME" ] || [ -e "$MUSIC_DISK_FILE_NAME" ] || [ -e "$LIGHTSHOW_DISK_FILE_NAME" ] || [ -e "$BOOMBOX_DISK_FILE_NAME" ] || [ -e "$BACKINGFILES_MOUNTPOINT/snapshots" ]
-then
-  if [ -t 0 ]
+function closeenough () {
+  DIFF=$(($1-$2))
+  if [ $DIFF -ge 0 ] && [ $DIFF -lt 10240 ]
   then
-    read -r -p 'Delete snapshots and recreate recording and music drives? (yes/cancel)' answer
-    case ${answer:0:1} in
-      y|Y )
-      ;;
-      * )
-        log_progress "aborting"
-        exit
-      ;;
-    esac
+    true
+    return
+  elif [ $DIFF -lt 0 ] && [ $DIFF -gt -10240 ]
+  then
+    true
+    return
   fi
-fi
-killall archiveloop || true
-/root/bin/disable_gadget.sh || true
-umount -d /mnt/cam || true
-umount -d /mnt/music || true
-umount -d /mnt/lightshow || true
-umount -d /mnt/boombox || true
-umount -d /backingfiles/snapshots/snap*/mnt || true
-rm -f "$CAM_DISK_FILE_NAME"
-rm -f "$MUSIC_DISK_FILE_NAME"
-rm -f "$LIGHTSHOW_DISK_FILE_NAME"
-rm -f "$BOOMBOX_DISK_FILE_NAME"
-rm -rf "$BACKINGFILES_MOUNTPOINT/snapshots"
+  false
+}
+
+function image_size_kb () {
+  echo $(($(stat --printf="%s" "$1")/1024))
+}
+
+function release_all_images () {
+  systemctl stop teslausb || true
+  killall archiveloop || true
+  /root/bin/disable_gadget.sh || true
+  umount -d /mnt/cam || true
+  umount -d /mnt/music || true
+  umount -d /mnt/lightshow || true
+  umount -d /mnt/boombox || true
+  umount -d /backingfiles/snapshots/snap*/mnt || true
+}
+
+function image_matches_params () {
+  local image_file="$1"
+  local requested_image_size="$2"
+
+  if [ "$requested_image_size" -gt 0 ]
+  then
+    if [ -e "$image_file" ]
+    then
+      local current_image_size=$(image_size_kb "$image_file")
+      if ! closeenough "$requested_image_size" "$current_image_size"
+      then
+        log_progress "$image_file should be resized (to $requested_image_size from $current_image_size)"
+        return 1
+      fi
+      # TODO check if filesystem matches
+    else
+      log_progress "$image_file should be created"
+      return 1
+    fi
+  else
+    if [ -e "$image_file" ]
+    then
+      log_progress "$image_file should be deleted"
+      return 1
+    fi
+  fi
+  return 0
+}
 
 # Check if kernel supports ExFAT 
 if ! check_for_exfat_support
@@ -212,63 +237,96 @@ then
   apt-get -y --force-yes install dosfstools
 fi
 
+CAM_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/cam_disk.bin"
+MUSIC_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/music_disk.bin"
+LIGHTSHOW_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/lightshow_disk.bin"
+BOOMBOX_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/boombox_disk.bin"
+
 CAM_DISK_SIZE="$(calc_size CAM_SIZE)"
 MUSIC_DISK_SIZE="$(calc_size MUSIC_SIZE)"
 LIGHTSHOW_DISK_SIZE="$(calc_size LIGHTSHOW_SIZE)"
 BOOMBOX_DISK_SIZE="$(calc_size BOOMBOX_SIZE)"
 
+if image_matches_params "$CAM_DISK_FILE_NAME" "$CAM_DISK_SIZE" && \
+   image_matches_params "$MUSIC_DISK_FILE_NAME" "$MUSIC_DISK_SIZE" && \
+   image_matches_params "$LIGHTSHOW_DISK_FILE_NAME" "$LIGHTSHOW_DISK_SIZE" && \
+   image_matches_params "$BOOMBOX_DISK_FILE_NAME" "$BOOMBOX_DISK_SIZE"
+then
+  log_progress "No need to update disk images"
+  exit 0
+fi
+
+# reduce the value of the given variable by 5%, but only until the specified minimum is reached
+function reduce_size () {
+  local curval="${!1}"
+  local minval=$(( $(dehumanize "$2") / 1024))
+  if [ "$curval" -le "$minval" ]
+  then
+    return
+  fi
+  local newval=$((curval*95/100))
+  if [ "$newval" -ge "$minval" ]
+  then
+    export $1=$newval
+  else
+    export $1=$minval
+  fi
+  adjusted=true
+}
+
 if [ "$((CAM_DISK_SIZE+MUSIC_DISK_SIZE+LIGHTSHOW_DISK_SIZE+BOOMBOX_DISK_SIZE))" -gt "$(available_space)" ]
 then
-  echo "Total requested size exceeds available space"
-  # TODO: reduce sizes to fit
-  exit 1
+  log_progress "Total requested size exceeds available space"
+  
+  while [ "$((CAM_DISK_SIZE+MUSIC_DISK_SIZE+LIGHTSHOW_DISK_SIZE+BOOMBOX_DISK_SIZE))" -gt "$(available_space)" ]
+  do
+    adjusted=false
+    reduce_size CAM_DISK_SIZE "30G"
+    reduce_size MUSIC_DISK_SIZE "4G"
+    reduce_size LIGHTSHOW_DISK_SIZE "1G"
+    reduce_size BOOMBOX_DISK_SIZE "500M"
+    if [ "$adjusted" = "false" ]
+    then
+      log_progress "Failed to adjust sizes to fit available space"
+      exit 1
+    fi
+  done
+  log_progress "Adjusted sizes to ${CAM_DISK_SIZE}K / ${MUSIC_DISK_SIZE}K / ${LIGHTSHOW_DISK_SIZE}K / ${BOOMBOX_DISK_SIZE}K"
 fi
 
-if [ "$CAM_DISK_SIZE" -gt 0 ]
+# if we get here, one or more of the images need to be created, deleted or updated, and there should be
+# enough space to do so, possibly requiring deleting some or all of the snapshots to free up space first.
+
+# TODO: resize images where possible, instead of recreating them
+if [ -e "$CAM_DISK_FILE_NAME" ] || [ -e "$MUSIC_DISK_FILE_NAME" ] || [ -e "$LIGHTSHOW_DISK_FILE_NAME" ] || [ -e "$BOOMBOX_DISK_FILE_NAME" ] || [ -e "$BACKINGFILES_MOUNTPOINT/snapshots" ]
 then
-  add_drive "cam" "CAM" "$CAM_DISK_SIZE" "$CAM_DISK_FILE_NAME" "$USE_EXFAT"
-  log_progress "created camera backing file"
-else
-  log_progress "skipping camera backing file"
+  if [ -t 0 ]
+  then
+    read -r -p 'Delete and recreate drives? (yes/cancel)' answer
+    case ${answer:0:1} in
+      y|Y )
+      ;;
+      * )
+        log_progress "aborting"
+        exit 1
+      ;;
+    esac
+  fi
 fi
 
-REMAINING_SPACE="$(available_space)"
+# shut down everything that might be using any of the drive images
+release_all_images
 
-if [ "$MUSIC_DISK_SIZE" -gt "$REMAINING_SPACE" ]
+add_drive "cam" "CAM" "$CAM_DISK_SIZE" "$CAM_DISK_FILE_NAME" "$USE_EXFAT"
+if [ "$CAM_DISK_SIZE" -eq 0 ]
 then
-  MUSIC_DISK_SIZE="$REMAINING_SPACE"
+  rm -rf "$BACKINGFILES_MOUNTPOINT/snapshots" &> /dev/null
 fi
 
-if [ "$REMAINING_SPACE" -ge 1024 ] && [ "$MUSIC_DISK_SIZE" -gt 0 ]
-then
-  add_drive "music" "MUSIC" "$MUSIC_DISK_SIZE" "$MUSIC_DISK_FILE_NAME" "$USE_EXFAT"
-  log_progress "created music backing file"
-fi
+add_drive "music" "MUSIC" "$MUSIC_DISK_SIZE" "$MUSIC_DISK_FILE_NAME" "$USE_EXFAT"
 
-REMAINING_SPACE="$(available_space)"
+add_drive "lightshow" "LIGHTSHOW" "$LIGHTSHOW_DISK_SIZE" "$LIGHTSHOW_DISK_FILE_NAME" "$USE_EXFAT"
 
-if [ "$LIGHTSHOW_DISK_SIZE" -gt "$REMAINING_SPACE" ]
-then
-  LIGHTSHOW_DISK_SIZE="$REMAINING_SPACE"
-fi
-
-if [ "$REMAINING_SPACE" -ge 1024 ] && [ "$LIGHTSHOW_DISK_SIZE" -gt 0 ]
-then
-  add_drive "lightshow" "LIGHTSHOW" "$LIGHTSHOW_DISK_SIZE" "$LIGHTSHOW_DISK_FILE_NAME" "$USE_EXFAT"
-  log_progress "created lightshow backing file"
-fi
-
-REMAINING_SPACE="$(available_space)"
-
-if [ "$BOOMBOX_DISK_SIZE" -gt "$REMAINING_SPACE" ]
-then
-  BOOMBOX_DISK_SIZE="$REMAINING_SPACE"
-fi
-
-if [ "$REMAINING_SPACE" -ge 1024 ] && [ "$BOOMBOX_DISK_SIZE" -gt 0 ]
-then
-  add_drive "boombox" "BOOMBOX" "$BOOMBOX_DISK_SIZE" "$BOOMBOX_DISK_FILE_NAME" "$USE_EXFAT"
-  log_progress "created boombox backing file"
-fi
+add_drive "boombox" "BOOMBOX" "$BOOMBOX_DISK_SIZE" "$BOOMBOX_DISK_FILE_NAME" "$USE_EXFAT"
 
 log_progress "done"
